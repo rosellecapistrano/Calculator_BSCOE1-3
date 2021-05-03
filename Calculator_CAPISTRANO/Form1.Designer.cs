417cf135576f67733d2ab0a14b0dedf42b64082a
Roselle Bianca Capistrano <robi0309.rbc6@gmail.com>
2021-05-03 16:45:01 +0800

    Update

--- Calculator_CAPISTRANO/Form1.Designer.cs
+++ Calculator_CAPISTRANO/Form1.Designer.cs
@@ -54,7 +54,7 @@ namespace Calculator_CAPISTRANO
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 61);
             this.button2.TabIndex = 1;
-            this.button2.Text = "0";
+            this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -63,7 +63,7 @@ namespace Calculator_CAPISTRANO
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 61);
             this.button3.TabIndex = 2;
-            this.button3.Text = "0";
+            this.button3.Text = "5";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -72,7 +72,7 @@ namespace Calculator_CAPISTRANO
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 61);
             this.button4.TabIndex = 3;
-            this.button4.Text = "0";
+            this.button4.Text = "8";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -81,7 +81,6 @@ namespace Calculator_CAPISTRANO
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 61);
             this.button5.TabIndex = 4;
-            this.button5.Text = "0";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
@@ -90,7 +89,7 @@ namespace Calculator_CAPISTRANO
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 61);
             this.button6.TabIndex = 5;
-            this.button6.Text = "0";
+            this.button6.Text = "=";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
@@ -100,7 +99,7 @@ namespace Calculator_CAPISTRANO
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 61);
             this.button7.TabIndex = 6;
-            this.button7.Text = "0";
+            this.button7.Text = ".";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // Form1
