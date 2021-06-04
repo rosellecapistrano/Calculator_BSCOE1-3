358ca97e2cafc695994704c60f4622a38a63d598
Roselle Bianca Capistrano <robi0309.rbc6@gmail.com>
2021-06-04 10:32:53 +0800

    Contact Tracing App

---
+++ contact tracing app/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class contact_tracing_app
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.START = new System.Windows.Forms.Button();
+            this.userinput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // START
+            // 
+            this.START.Location = new System.Drawing.Point(120, 293);
+            this.START.Name = "START";
+            this.START.Size = new System.Drawing.Size(130, 43);
+            this.START.TabIndex = 0;
+            this.START.Text = "START";
+            this.START.UseVisualStyleBackColor = true;
+            this.START.Click += new System.EventHandler(this.START_Click);
+            // 
+            // userinput
+            // 
+            this.userinput.Location = new System.Drawing.Point(46, 60);
+            this.userinput.Name = "userinput";
+            this.userinput.Size = new System.Drawing.Size(294, 96);
+            this.userinput.TabIndex = 1;
+            this.userinput.Text = "";
+            // 
+            // contact_tracing_app
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 355);
+            this.Controls.Add(this.userinput);
+            this.Controls.Add(this.START);
+            this.Name = "contact_tracing_app";
+            this.Text = "Contact Tracing App";
+            this.Load += new System.EventHandler(this.contact_tracing_app_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button START;
+        private System.Windows.Forms.RichTextBox userinput;
+    }
+}
+
---
+++ contact tracing app/WinFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class contact_tracing_app : Form
+    {
+        public contact_tracing_app()
+        {
+            InitializeComponent();
+        }
+
+        private void START_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contact_tracing_app_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
